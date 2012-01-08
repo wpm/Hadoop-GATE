ec1ac88d9e0d96291700bd6cee2056fdb0c13fe9
W.P. McNeill <billmcn@gmail.com>
2012-01-07 17:31:36 -0800

    Create a Hadoop Job that Runs GATE
    Create a simple Hadoop job that runs a GATE app. So far this copies a zipped .gapp file
    into the distributed cache and unzips it on the task nodes but doesn't do anything with it.

---
+++ src/main/java/wpmcn.hadoop/HadoopGATE.java
@@ -0,0 +1,75 @@
+package wpmcn.hadoop;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+
+public class HadoopGATE extends Configured implements Tool {
+   private static final String HDFS_GATE_APP = "/tmp/gate-app.zip";
+
+   static public class HadoopGATEMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+      private Path gateAPP;
+
+      @Override
+      protected void setup(Context context) throws IOException, InterruptedException {
+         Configuration configuration = context.getConfiguration();
+         Path[] localCache = DistributedCache.getLocalCacheArchives(configuration);
+         gateAPP = localCache[0];
+      }
+
+      @Override
+      protected void map(LongWritable offset, Text text, Context context) throws IOException, InterruptedException {
+         context.setStatus(gateAPP.toString());
+         System.out.println(gateAPP +  ": <" + offset + " " + text + ">");
+         context.write(offset, text);
+      }
+   }
+
+   static public Job createJob(Configuration configuration, String[] args) throws IOException {
+      Path gateApp = new Path(args[0]);
+      Path input = new Path(args[1]);
+      Path output = new Path(args[2]);
+      // Put the GATE application into the distributed cache.
+      FileSystem fs = FileSystem.get(configuration);
+      Path hdfsGateApp = new Path(HDFS_GATE_APP);
+      fs.copyFromLocalFile(gateApp, hdfsGateApp);
+      DistributedCache.addCacheArchive(hdfsGateApp.toUri(), configuration);
+
+      Job job = new Job(configuration, "GATE " + output);
+      FileInputFormat.addInputPath(job, input);
+      SequenceFileOutputFormat.setOutputPath(job, output);
+
+      job.setJarByClass(HadoopGATE.class);
+      job.setInputFormatClass(TextInputFormat.class);
+      job.setOutputFormatClass(SequenceFileOutputFormat.class);
+
+      job.setMapperClass(HadoopGATEMapper.class);
+
+      return job;
+   }
+
+   public int run(String[] args) throws Exception {
+      Job job = createJob(getConf(), args);
+      boolean success = job.waitForCompletion(true);
+      if (success)
+         FileSystem.get(job.getConfiguration()).deleteOnExit(new Path(HDFS_GATE_APP));
+      return success ? 0 : -1;
+   }
+
+   static public void main(String[] args) throws Exception {
+      System.exit(ToolRunner.run(new HadoopGATE(), args));
+   }
+}
